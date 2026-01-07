4cc02de91cfd4e82be140cc658f727527f121e08
alexVinchent <alexvinchent312.talenta@gmail.com>
2026-01-07 18:11:56 +0700

    Completed 50-page scraping loop

--- scrapper.py
+++ scrapper.py
@@ -1,27 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
-url = 'https://books.toscrape.com/'
-
-response = requests.get(url)
-page_html = response.text
-
-soup = BeautifulSoup(page_html, 'html.parser')
-books = soup.find_all("article", class_="product_pod")
+import time
+# url = 'https://books.toscrape.com/'
 
 book_data=[]
-for book in books:
-    name = book.h3.find("a")['title']
-    price = book.find("p", class_="price_color").text[2:]
+for page_number in range(1,51):
+    url = f"https://books.toscrape.com/catalogue/page-{page_number}.html"
+    response = requests.get(url)
+    response.encoding = 'utf-8'
 
-    book_data.append({
-        'Title':name,
-        'Price':price
-    })
+    if response.status_code != 200:
+        break
+
+    page_html = response.text
+
+    soup = BeautifulSoup(page_html, 'html.parser')
+    books = soup.find_all("article", class_="product_pod")
+
+    print(f"Scraping page {page_number}...")
+    for book in books:
+        name = book.h3.find("a")['title']
+        price = book.find("p", class_="price_color").text[2:]
+
+        book_data.append({
+            'Title':name,
+            'Price':price
+        })
+    time.sleep(1)
 
 df = pd.DataFrame(book_data)
 df.to_csv('book.csv', index=False)
 
-print("Success! Data saved to books.csv")
-print(df.head())
+print("Success! Data saved to book.csv")
+print(df.shape)
